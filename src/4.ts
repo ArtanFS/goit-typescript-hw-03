10b4187ecc15ba60b5f7995a0293a2c1a57c218f
ArtanFS <132677965+ArtanFS@users.noreply.github.com>
2024-01-03 00:29:58 +0200

    make task 04

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,62 @@
-const key = new Key();
+class Key {
+  private signature: number;
 
-const house = new MyHouse(key);
+  constructor() {
+    this.signature = Math.random();
+  }
+  public getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+  public getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  door: boolean = false;
+  key: Key;
+  tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("You can enter the house.");
+    } else {
+      console.log("You can't enter the house.");
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key === this.key) {
+      this.door = true;
+      console.log("Correct key. The door is open.");
+    } else {
+      console.log("Incorrect key. The door is close.");
+    }
+  }
+}
+
+const key = new Key();
 const person = new Person(key);
+const house = new MyHouse(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
 
 export {};
